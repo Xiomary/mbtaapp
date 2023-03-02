700b4e06017869e35c3b2a0a1ca89443c943ef48
xiomary <Xiomary331@gmail.com>
2023-03-02 09:35:27 -0500

    updated frontend

--- frontend/src/components/pages/routePatterns.js
+++ frontend/src/components/pages/routePatterns.js
@@ -1,30 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
+ 
 
-function RoutePattern() {
-  const [routePatterns, setRoutePatterns] = useState([]);
+function Route() {
+  const [route, setRoutes] = useState([]);
+ 
+ 
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios('https://api-v3.mbta.com/route_patterns');
-      setRoutePatterns(result.data.data);
+      const result = await axios(
+        'https://api-v3.mbta.com/route_patterns',
+      );
+      setRoutes(result.data.data);
     }
     fetchData();
   }, []);
-
+ 
   return (
+
     <div>
-      <h1>Route Patterns</h1>
-      <ul>
-        {routePatterns.map(routePattern => (
-          <li key={routePattern.id}>
-            <h3>{routePattern.attributes.name}</h3>
-            <p>{routePattern.attributes.effect}</p>
-          </li>
-        ))}
-      </ul>
+      {route.map(route => (
+        <Card
+        body
+        bg="info"
+        text="dark"
+        border="warning"
+        className="mx-1 my-2"
+        style={{ width: "30rem" }}
+        
+      >
+        <Card.Body>
+        <Card.Title>RoutePatterns</Card.Title>
+        <Card.Text>{route.attributes.name}{route.attributes.effect}</Card.Text>
+        </Card.Body>
+        
+      </Card>
+      ))}
+ 
+    
+      {route.map(route => (
+        <div key={route.id}>
+          <h3>{route.attributes.name}</h3>
+          <p>{route.attributes.effect}</p>
+        </div>
+      ))}
     </div>
   );
+ 
+  
 }
 
-export default RoutePattern;
+export default Route;
+ 
+
+ 
